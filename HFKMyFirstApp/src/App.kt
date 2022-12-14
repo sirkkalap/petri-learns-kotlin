a4bdfa0858023af38edba1ec8da9c99650e67f33
Petri Sirkkala <petri.sirkkala@solita.fi>
2022-12-14 15:31:55 +0200

    Value of outer variables remain after loop

--- HFKMyFirstApp/src/App.kt
+++ HFKMyFirstApp/src/App.kt
@@ -39,4 +39,12 @@ fun main() {
         println(z)
         z += 6
     }
+
+    x=1
+    println("Before the loop. x = $x.")
+    while(x<4){
+        println("In the loop x = $x.")
+        x+=1
+    }
+    println("After the loop. x = $x.")
 }
