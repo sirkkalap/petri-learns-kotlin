1fc621d534e5d7eeca489d6e6a32d2893591a159
Petri Sirkkala <petri.sirkkala@solita.fi>
2022-12-14 14:00:09 +0200

    Conditionals

--- HFKMyFirstApp/src/App.kt
+++ HFKMyFirstApp/src/App.kt
@@ -1,18 +1,28 @@
 fun main() {
-    val name = "Petri"
-    println("Pow! My name is $name")
+    println("Pow!")
 
     var x = 3
+    val name = "Petri"
     x *= 10
     // Hmmm. Kommentti.
     print("x is $x")
 
-    while ( x > 20) {
+    while (x > 20) {
         x -= 1
         print(" x is now $x")
     }
     for (i in 1..10) {
         x += 1
         print(" x is now $x")
+    }
+
+    if (x == 20) {
+        println(" x must be 20.")
+    } else {
+        println(" x isn't 20.")
+    }
+
+    if (name == "Petri") {
+        println("$name Strike")
     }
 }
