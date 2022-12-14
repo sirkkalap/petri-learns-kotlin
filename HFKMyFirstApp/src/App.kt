5bcafa0f6dca0be0ac3bcdbc07d3ffa7ed02600d
Petri Sirkkala <petri.sirkkala@solita.fi>
2022-12-14 13:55:38 +0200

    Looping

--- HFKMyFirstApp/src/App.kt
+++ HFKMyFirstApp/src/App.kt
@@ -1,8 +1,18 @@
 fun main() {
-    var x = 3
     val name = "Petri"
-    x *= 10
-    print("x is $x")
-    // Hmmm. Kommentti.
     println("Pow! My name is $name")
+
+    var x = 3
+    x *= 10
+    // Hmmm. Kommentti.
+    print("x is $x")
+
+    while ( x > 20) {
+        x -= 1
+        print(" x is now $x")
+    }
+    for (i in 1..10) {
+        x += 1
+        print(" x is now $x")
+    }
 }
